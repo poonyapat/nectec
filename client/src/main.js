09c9f90435febcc17ce81fa11a7be8ac7207e410
Zyle <poonyapat.y@ku.th>
2018-08-30 00:16:28 +0700

    add vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
+import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.indigo.darken1,
+    secondary: colors.grey.darken1
+  }
+})
+
+sync(store, router)
 
 new Vue({
   router,
