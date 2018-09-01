018ca68b32f7abee261f4641fe0c677d075fd792
Zyle <poonyapat.y@ku.th>
2018-09-01 10:32:19 +0700

    add temple

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,9 @@
 const TestService = require('./controllers/testController')
+const TemplaeMatchingController = require('./controllers/temple/templeMatchingController')
+const ArchiologicalSiteController = require('./controllers/archiologicalSiteController')
 
 module.exports = (app) => {
     app.get('/test', TestService.test)
+    app.get('/temple', TemplaeMatchingController.getTempleData)
+    app.get('/archiology', ArchiologicalSiteController.getArchiologicalSite)
 }
