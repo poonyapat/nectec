6f42ef459387d666b7dbdafafc7d40b4af93e50a
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2018-09-01 13:51:36 +0700

    Move Card and Content to component

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,12 +5,15 @@
         <b-navbar toggleable="md" type="dark" variant="info">
           <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-          <b-navbar-brand href="#">NavBar</b-navbar-brand>
+          <b-navbar-brand href="#">TNTD.COM</b-navbar-brand>
 
           <b-collapse is-nav id="nav_collapse">
 
             <b-navbar-nav>
-              <b-nav-item href="#">Link</b-nav-item>
+              <b-nav-item href="#">Content</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav>
+              <b-nav-item href="#">เบอร์ฉุกเฉิน</b-nav-item>
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
@@ -21,14 +24,6 @@
                 <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
               </b-nav-form>
     
-              <b-nav-item-dropdown right>
-                <!-- Using button-content slot -->
-                <template slot="button-content">
-                  <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Signout</b-dropdown-item>
-              </b-nav-item-dropdown>
             </b-navbar-nav>
 
           </b-collapse>
@@ -88,6 +83,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+@import url('https://fonts.googleapis.com/css?family=Kanit');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
