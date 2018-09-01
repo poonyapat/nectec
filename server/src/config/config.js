79356e43c522d612e085e48c096c9a38e15c0901
Unknown <ploy_mel@hotmail.com>
2018-09-01 10:02:02 +0700

    Google Map, Places API, MongoDB, GG Trends

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    port: process.env.PORT || '8081'
+    port: process.env.PORT || '8081',
+    mongoURI: 'mongodb://ploymel:poynoy12@ds239692.mlab.com:39692/nectec'
 }
--- server/src/controllers/testController.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    test(req,res){
-        res.send({
-            message: 'test'
-        })
-    }
-}
